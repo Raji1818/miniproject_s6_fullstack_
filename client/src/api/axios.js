d501b5b1ee87032216f390e13bc8ea23838a794a
Ramya <rajeswari8400@gmail.com>
2026-04-20 22:39:55 +0530

    update API URL to use studentdev-backend for consistency

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const defaultApiUrl = 'https://studentdev-backend.onrender.com/api';
 const configuredApiUrl = process.env.REACT_APP_API_URL?.replace(/\/+$/, '');
 
 const api = axios.create({
-  baseURL: configuredApiUrl || 'http://localhost:5000/api'
+  baseURL: configuredApiUrl || defaultApiUrl
 });
 
 api.interceptors.request.use((config) => {
